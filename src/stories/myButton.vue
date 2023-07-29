37ed3b28a8e7d8a0e6b2906bcd3f25abf6fdb10e
shunichi <khf04440@nifty.com>
2023-07-29 09:23:44 +0900

    storybook installed

---
+++ src/stories/myButton.vue
@@ -0,0 +1,54 @@
+<template>
+  <button type="button" :class="classes" @click="onClick" :style="style">
+    {{ label }}
+  </button>
+</template>
+
+<script lang="ts" setup>
+import "./button.css";
+import { computed } from "vue";
+
+// eslint-disable-next-line no-undef
+const props = withDefaults(
+  // eslint-disable-next-line no-undef
+  defineProps<{
+    /**
+     * The label of the button
+     */
+    label: string;
+    /**
+     * primary or secondary button
+     */
+    primary?: boolean;
+    /**
+     * size of the button
+     */
+    size?: "small" | "medium" | "large";
+    /**
+     * background color of the button
+     */
+    backgroundColor?: string;
+  }>(),
+  { primary: false }
+);
+
+// eslint-disable-next-line no-undef
+const emit = defineEmits<{
+  (e: "click", id: number): void;
+}>();
+
+const classes = computed(() => ({
+  "storybook-button": true,
+  "storybook-button--primary": props.primary,
+  "storybook-button--secondary": !props.primary,
+  [`storybook-button--${props.size || "medium"}`]: true,
+}));
+
+const style = computed(() => ({
+  backgroundColor: props.backgroundColor,
+}));
+
+const onClick = () => {
+  emit("click", 1);
+};
+</script>
